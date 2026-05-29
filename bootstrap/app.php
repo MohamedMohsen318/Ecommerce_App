5738cd5de6d0c92c5aff155a5cdec7f26eae9731
Mohamed Mohsen <Mohamedsn101@gmail.com>
2026-05-30 00:36:51 +0300

    Add admin guard implementation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,31 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(
+            fn ($request) => $request->is('admin/*') ? route('admin.login') : route('login')
+        );
+
+        $middleware->redirectUsersTo(
+            fn ($request) => $request->is('admin/*') ? route('admin.dashboard') : route('welcome')
+        );
+
+        $middleware->alias([
+            'superadmin' => \App\Http\Middleware\CheckSuperAdmin::class,
+        ]);
+
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->respond(function ($response) {
+            if ($response->getStatusCode() === 419) {
+                return redirect()->route(
+                    request()->is('admin/*') ? 'admin.login' : 'login'
+                );
+            }
+
+            return $response;
+        });
+    })
+    ->create();
